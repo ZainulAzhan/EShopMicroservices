0d2ac44d38635c34814274cac3cb6364b4f25b15
Zainul Azhan <zainulazhan2014@gmail.com>
2025-05-27 22:36:01 +0800

    finish Catalog.API CRUD operations

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,20 @@
+﻿namespace Catalog.API.Products.GetProducts;
+
+public record GetProductsResponse(IEnumerable<Product> Products);
+public class GetProductsEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products", async (ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductsQuery());
+            var response = result.Adapt<GetProductsResponse>();
+            return Results.Ok(response);
+        })
+            .WithName("GetProducts")
+            .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Products")
+            .WithDescription("Retrieves a list of products from the catalog. The response includes product details such as name, category, description, image file, and price.");
+    }
+}
