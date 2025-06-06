368b8aa46a3bfb3c53f61336d44535f5a1e07608
Zainul Azhan <zainulazhan2014@gmail.com>
2025-06-07 07:01:37 +0800

    Add Ordering API project

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Aggregate.cs
@@ -0,0 +1,22 @@
+﻿namespace Ordering.Domain.Abstractions;
+
+public abstract class Aggregate<T> : Entity<T>, IAggregate<T>
+    where T : notnull
+{
+    private readonly List<IDomainEvent> _domainEvents = new();
+    public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+    public void AddDomainEvent(IDomainEvent domainEvent)
+    {
+        if (domainEvent == null)
+        {
+            throw new ArgumentNullException(nameof(domainEvent), "Domain event cannot be null.");
+        }        
+        _domainEvents.Add(domainEvent);
+    }
+    public IDomainEvent[] ClearDomainEvents()
+    {
+        var events = _domainEvents.ToArray();
+        _domainEvents.Clear();
+        return events;
+    }
+}
